927cd0fef73143c3d03e98ffe29cdd65899dc3ef
Rouzok <98026745+Rouzok@users.noreply.github.com>
2026-03-29 22:13:19 +0700

    menambahkan class SimpleDataBase

--- modul5_103082400009/Program.cs
+++ modul5_103082400009/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace TugasGeneric
+{
+    public class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            this.storedData = new List<T>();
+            this.inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T data)
+        {
+            this.storedData.Add(data);
+            this.inputDates.Add(DateTime.UtcNow);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, yang disimpan pada waktu UTC: {inputDates[i]}");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleDataBase<long> db = new SimpleDataBase<long>();
+            db.AddNewData(10L);
+            db.AddNewData(30L);
+            db.AddNewData(82L);
+
+            db.PrintAllData();
+        }
+    }
+}
